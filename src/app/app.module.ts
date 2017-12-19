616f899b5f8bc4aeddad45a1db583a3d92e725a0
xortiz <xavier.ortiz.ch@gmail.com>
2017-12-19 15:50:03 -0500

    added bootstrap modules, calculator component, and added a rudimentary GUI for the calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { CalculatorComponent } from './calculator/calculator.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {}
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
